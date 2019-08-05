9a40481e3798b48cd8387310f36b0fad48599822
Fraccas <daltonward1@yahoo.com>
2019-08-05 15:01:04 -0500

    Finished Reacting to React

--- reacting-to-react/src/components/App.jsx
+++ reacting-to-react/src/components/App.jsx
@@ -1,5 +1,65 @@
 import React, {Component} from 'react';
 
-const App = (props) => {
-    return <h1>Hello, {props.name}</h1>;
+// const App = (props) => {
+//     return <h1>Hello, {props.name}!</h1>;
+// }
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: 'Fraccas',
+            hasLoaded: false
+        };
+    }
+    
+    handleInputChange = (name) => {
+        this.setState({name});
+    }
+
+    handleClick = (loaded) => {
+        this.setState({
+            hasLoaded: loaded
+        });
+    }
+
+    componentDidMount = () => {
+        this.setState({
+            hasLoaded: true
+        });
+    }
+
+    render() {
+        if (this.state.hasLoaded) {
+            return (
+                <React.Fragment>
+                    <h1>{this.props.msg} {this.state.name}!</h1>
+                    <input
+                        value={this.state.name}
+                        placeholder={'enter name'}
+                        onChange={(event) => this.handleInputChange(event.target.value)}
+                    /><br></br>
+                    <button
+                        onClick={(event) => this.handleClick(false)}
+                    >
+                        CheckLoaded
+                    </button>
+                </React.Fragment>
+            );
+        } else {
+            return (
+                <React.Fragment>
+                    <h1>Loading...</h1>
+                    <button
+                        onClick={(event) => this.handleClick(true)}
+                    >
+                        CheckLoaded
+                    </button>
+                </React.Fragment>
+            );
+        }
+    }
 }
+
+export default App;
---
+++ reacting-to-react/src/index.jsx
@@ -0,0 +1,5 @@
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDom.render(<App msg="Hi, my name is "/>, document.getElementById('root'));
