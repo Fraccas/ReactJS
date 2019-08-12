14bfaab62885cbe8d3cafb2b49e2a36b6b82359c
Fraccas <daltonward1@yahoo.com>
2019-08-12 12:20:49 -0500

    Finished Phase 2

--- reacting-to-apis/src/App.jsx
+++ reacting-to-apis/src/App.jsx
@@ -6,35 +6,51 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {films: null};
+        this.state = {
+            films: null, 
+            showFilms: false
+        };
     }
 
-    componentDidMount() {
+    LoadFilms() {
         fetch("https://ghibliapi.herokuapp.com/films")
         .then(res => res.json())
         .then(obj => {
-            this.setState({ films: obj })
-            console.log(this.state.films)
+            this.setState({ films: obj });
+            this.setState({showFilms: true});
         }).catch(e => console.log(e))  
     }
 
 
     render() {
-        return (
-            <>
-                <h1 className="bg-primary text-white p-2 text-center">Welcome to MovieDB</h1>
-                <div className="card-holder w-100 p-3"> 
-                {this.DisplayFilms()}
-                </div>
-            </>
-        );
+        if (this.state.showFilms) {
+            return (
+                <>
+                    <div className="bg-dark">
+                        <h1 className="bg-primary text-white p-2 text-center">Welcome to MovieDB</h1>
+                        <div className="card-holder w-100 p-3"> 
+                        {this.DisplayFilms()}
+                    </div>
+                    </div>
+                </>
+            );
+        } else {
+            return (
+                <>
+                    <div className="bgi">
+                        
+                    </div>
+                    <button onClick={() => { this.LoadFilms() }} id="filmB" type="button" className="btn btn-primary btn-lg btn-block my-3">Load Films</button>
+                    <button id="peopleB" type="button" className="btn btn-dark btn-lg btn-block my-3">Load People</button>
+                </>
+              );
+        }
     }
 
     DisplayFilms() {
-        if (this.state.films) {
+        if (this.state.films && this.state.showFilms) {
             return (
                 this.state.films.map((value, index) => {
-                    console.log(value.id);
                     return <MovieCard key={value.id} movie={value}></MovieCard>
                 })
             );
